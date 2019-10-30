fc0ff31ec1a0cd535a67d0792be0d1385899335d
Neal Sabin <sabin7@mail.nmc.edu>
2019-10-30 15:11:16 -0400

    Add project files.

---
+++ HelloNMC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello NMC");
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.Write("Press any key to exit.");
+        }
+    }
+}
